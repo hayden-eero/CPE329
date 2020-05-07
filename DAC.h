3e70e37b43ea511c9aef8cff662f57f2bf1eefd0
goant99 <62232340+goant99@users.noreply.github.com>
2020-05-07 15:50:28 -0700

    Create DAC.h

---
+++ DAC.h
@@ -0,0 +1,16 @@
+/*
+ * DAC.h
+ *
+ *  Created on: May 7, 2020
+ *      Author: Hayden
+ */
+
+#ifndef DAC_H_
+#define DAC_H_
+
+void startDAC();
+void dataDAC(uint16_t data);
+void setSquare();
+
+
+#endif /* DAC_H_ */
