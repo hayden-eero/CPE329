e6d62c4939d5cf2d2bd765847aca79a92eaf069e
goant99 <62232340+goant99@users.noreply.github.com>
2020-05-14 21:19:57 -0700

    Create P2delay.h

---
+++ P2delay.h
@@ -0,0 +1,25 @@
+/*
+ * delay.h
+ *
+ *  Created on: Apr 10, 2020
+ *      Author: Hayden
+ */
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+//declaring the values with their respective frequencies
+
+#define FREQ_1_5_Mhz 1500000
+#define FREQ_3_Mhz 3000000
+#define FREQ_6_Mhz 6000000
+#define FREQ_12_Mhz 12000000
+#define FREQ_24_Mhz 24000000
+#define FREQ_48_Mhz 48000000
+
+
+//declaring the functions to be used
+
+void set_DCO(int freq);
+
+#endif /* DELAY_H_ */
