6bc9517f539aec596cb794f6115feda9fabf6dbe
goant99 <62232340+goant99@users.noreply.github.com>
2020-06-08 12:57:02 -0700

    Create finalMotorController.c

---
+++ finalMotorController.c
@@ -0,0 +1,14 @@
+/*
+ * MotorController.h
+ *
+ *  Created on: May 31, 2020
+ *      Author: Hayden
+ */
+
+#ifndef MOTORCONTROLLER_H_
+#define MOTORCONTROLLER_H_
+
+void squareOutput(double frequency, int dutyCycleTotal);
+void squareOutputSound(double frequency, int dutyCycleTotal);
+
+#endif /* MOTORCONTROLLER_H_ */
